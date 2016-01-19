505b71ad838b963a97ba5781cc6aecb85ea20fc1
alex-ubuntu <uwalex@gmail.com>
2016-01-19 16:42:34 -0500

    LAST COMMIT K

--- lab1/massdiff.sh
+++ lab1/massdiff.sh
@@ -5,11 +5,11 @@ for file in *;
 do
     if ! diff -q $1 $file > /dev/null
     then
-	echo $1 "and " $file "are different"
+	echo $1 "and" $file "are different"
 	diff $1 $file >> "log.txt"
     else
 	#echo "file and file_name are the same"
-	echo $1 "and " $file "are the same"
+	echo $1 "and" $file "are the same"
     fi
 done
 
