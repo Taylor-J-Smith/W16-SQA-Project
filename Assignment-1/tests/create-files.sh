033b22699412fa2b35601876227bfb187a759e52
Taylor-J-Smith <taylor.smith1@uoit.net>
2016-02-11 11:18:27 -0500

    Update script to take two paramaters, directory name and number of files

--- Assignment-1/tests/create-files.sh
+++ Assignment-1/tests/create-files.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-for i in {1..6}
+if(( $# < 2)); then
+  echo "Please pass in two arguements, the directory name, and number of files"
+  exit
+fi
+
+DIRECTORY=$1
+numFiles=$2
+
+for (( i = 1 ; i <= $numFiles ; i++ ))
 do
-    touch logout/IN-logout_test$i.txt
-    touch logout/OUT-logout_test$i.txt
+  if [ ! -d "$DIRECTORY" ]; then
+    mkdir "$DIRECTORY"
+  fi
+  if(( $i < 10 )); then
+      touch "$DIRECTORY"/IN-"$DIRECTORY"_test0"$i".txt
+      touch "$DIRECTORY"/OUT-"$DIRECTORY"_test0"$i".txt
+  else
+    touch "$DIRECTORY"/IN-"$DIRECTORY"_test"$i".txt
+    touch "$DIRECTORY"/OUT-"$DIRECTORY"_test"$i".txt
+  fi
 done
