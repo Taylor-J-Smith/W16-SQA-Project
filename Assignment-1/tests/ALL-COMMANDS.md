0c913d8241dfcb3eba6508dc657029b24d0e5eee
alex-ubuntu <uwalex@gmail.com>
2016-02-13 16:55:02 -0500

    Progress in creating files for login

---
+++ Assignment-1/tests/ALL-COMMANDS.md
@@ -0,0 +1,62 @@
+Welcome! type login to begin:
+
+**Sample Transaction**
+CC_AAAAAAAAAAAAAAAAAAAA_NNNNN_PPPPPPPP_MM
+00 TESTUSER1            00000 00000.00 
+
+#Login
+
+#Logout
+[logout] Error: Need to log in first!
+
+#Withdrawal
+[withdrawal] Error: Need to log in first!
+
+#Transfer
+[transfer] Error: Need to log in first!
+
+#Paybill
+[paybill] Error: Need to log in first!
+
+#Deposit
+[deposit] Error: Need to log in first!
+
+#create
+[create] Error: Need to log in first!
+
+#Delete
+[delete] Error: Need to log in first!
+
+#Disable
+[disable] Error: Need to log in first!
+
+#Changeplan
+[changeplan] Error: Need to log in first!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Assignment-1/tests/create-files.sh
+++ Assignment-1/tests/create-files.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-if(( $# < 2)); then
-  echo "Please pass in two arguements, the directory name, and number of files"
-  exit
+if(( $# < 4)); then
+    echo "<Directory><file-name><type><numFiles>"
+    echo "example: ./create-files.sh login/in login in 9"
+    echo "example: ./create-files.sh login/out login out 9"
+    echo "example: ./create-files.sh login/trans login tf 9"
+    exit
 fi
 
 DIRECTORY=$1
-numFiles=$2
+fileName=$2
+type=$3
+numFiles=$4
 
 for (( i = 1 ; i <= $numFiles ; i++ ))
 do
-  if [ ! -d "$DIRECTORY" ]; then
-    mkdir "$DIRECTORY"
-  fi
-  if(( $i < 10 )); then
-      touch "$DIRECTORY"/IN-"$DIRECTORY"_test0"$i".txt
-      touch "$DIRECTORY"/OUT-"$DIRECTORY"_test0"$i".txt
-  else
-    touch "$DIRECTORY"/IN-"$DIRECTORY"_test"$i".txt
-    touch "$DIRECTORY"/OUT-"$DIRECTORY"_test"$i".txt
-  fi
+    if [ ! -d "$DIRECTORY" ]; then
+	echo mkdir "$DIRECTORY"
+	mkdir "$DIRECTORY"
+    fi
+    if(( $i < 10 )); then
+	echo touch "$DIRECTORY"/"$fileName"0"$i".$type
+	touch "$DIRECTORY"/"$fileName"0"$i".$type
+    else
+	echo touch "$DIRECTORY"/"$fileName$i".$type
+	touch "$DIRECTORY"/"$fileName$i".$type
+    fi
 done
