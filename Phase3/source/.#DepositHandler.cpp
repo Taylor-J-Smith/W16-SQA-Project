4056c3855d8933ddd65ee2194537470c9890f525
alex-ubuntu <uwalex@gmail.com>
2016-03-08 18:08:59 -0500

    Deposit 03-05 complete

---
+++ Phase3/source/.#DepositHandler.cpp
@@ -0,0 +1,1 @@
+alex-ubuntu@alexubuntu-Latitude-E5550.3615:1457464211
--- Phase3/source/DepositHandler.h
+++ Phase3/source/DepositHandler.h
@@ -10,13 +10,23 @@
 class DepositHandler: public TransactionHandler{
 
  private:
-  AccountConstants constants_;
+  AccountConstants static constants_;
 
  public:
   //Handles the addition of money into the provided bank account. A transaction is created and added to the session_transactions on successful completion of the deposit and finally the account database is updated to reflect the deposit transaction.
   void static handle(SessionStatus current_status, 
-		     AccountsDatabase account_database,
+		     AccountsDatabase &account_database,
 		     std::vector<Transaction> &session_transactions);
+
+  
+  //Update the deposit amount for the given account number
+  void static updateDepositAmount(AccountsDatabase &account_database,
+				  std::string account_number,
+				  std::string deposit_instance);
+  //Check if the amount to be deposit is possible with that account's balance
+  bool static isDepositPossible(AccountsDatabase &account_database,
+				std::string account_number,
+				std::string deposit_instance);
 };
 
 #endif
