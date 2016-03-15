c6d2d975a85fa040fbf03c40e0cb854fd90074b0
alex-ubuntu <uwalex@gmail.com>
2016-03-15 17:48:19 -0400

    Refactored switch statement to backEnd.java from TransactionHandler.java

--- Phase4/source/TransactionHandler.java
+++ Phase4/source/TransactionHandler.java
@@ -2,108 +2,78 @@ public class TransactionHandler{
 
     //members
     private static boolean isAdmin = false;
-    
-    public static void handle(Transaction t, BankAccounts b){
-
-	//Figure out the transaction type at question
-	switch (t.transactionType) {
-	case LOGOUT: 
-	    logout(t, b);
-	    break;
-	case WITHDRAWAL:
-	    withdrawal(t,b);
-	    break;
-	case TRANSFER:
-	    transfer(t,b);
-	    break;
-	case PAYBILL:
-	    paybill(t,b);
-	    break;
-	case DEPOSIT:
-	    deposit(t,b);
-	    break;
-	case CREATE:
-	    create(t,b);
-	    break;
-	case DELETE:
-	    delete(t,b);
-	    break;
-	case DISABLE: 
-	    disable(t,b);
-	    break;
-	case CHANGEPLAN: 
-	    changeplan(t,b);
-	    break;
-	case ENABLE:
-	    enable(t,b);
-	    break;
-	case LOGIN: 
-	    login(t,b);
-	    break; 
-	}    
-    }
   
-    private static void logout(Transaction t, BankAccounts b){
+    public static void logout(Transaction t, BankAccounts b){
 	System.out.println(t.transactionType);
 	//set the admin variable to false
 	isAdmin = false;
     }
   
-    private static void withdrawal(Transaction t, BankAccounts b){
+    public static void withdrawal(Transaction t, BankAccounts b){
     	System.out.println(t.transactionType);
 	//obtain the current account in question
 	Account currAccount = b.getAccount(t.accountNumber);
 	b.checkStatus(t.accountNumber); //temp
 	//remove the withdrawn amount
-	currAccount.balance_ -= t.fundsInvolved;
+	double withdrawalAmount = t.fundsInvolved;
 	//check if it is an admin
 	if (!isAdmin){
-	    //not an admin - apply fees
+	    //not an admin - apply fees	    
 	    if (currAccount.plan_.compareTo("S") == 0){
 		//is a student account
-		currAccount.balance_ -= 0.05;
+		withdrawalAmount += 0.05;
 	    }else{
 		//Not a student account
-		currAccount.balance_ -= 0.10;
+		withdrawalAmount += 0.10;
 	    }
-	}	
-	currAccount.num_trans_++;
+	}
+
+	//Check if the account balance is less than 0 once amount has been withdrawn
+	if (currAccount.balance_ - withdrawalAmount < 0){
+	    System.out.println("ERROR: Attempt to withdrawal more funds than possible");
+	}else{
+	    currAccount.balance_ -= withdrawalAmount;
+	    currAccount.num_trans_++; //increment the transaction number   
+	}
 	b.checkStatus(t.accountNumber); //temp
     }
 
-    private static void transfer(Transaction t, BankAccounts b){
-    	System.out.println(t.transactionType);
+    public static void transfer(Transaction t1, Transaction t2, BankAccounts b){
+	System.out.println(t1.transactionType);
+	b.checkStatus(t1.accountNumber); //temp
+	b.checkStatus(t2.accountNumber); //temp
+
 	
     }
 
-    private static void paybill(Transaction t, BankAccounts b){
+    public static void paybill(Transaction t, BankAccounts b){
     	System.out.println(t.transactionType);
     }
 
-    private static void deposit(Transaction t, BankAccounts b){
+    public static void deposit(Transaction t, BankAccounts b){
     	System.out.println(t.transactionType);
     }
 
-    private static void create(Transaction t, BankAccounts b){
+    public static void create(Transaction t, BankAccounts b){
     	System.out.println(t.transactionType);
     }
 
-    private static void delete(Transaction t, BankAccounts b){
+    public static void delete(Transaction t, BankAccounts b){
     	System.out.println(t.transactionType);
     }
 
-    private static void disable(Transaction t, BankAccounts b){
+    public static void disable(Transaction t, BankAccounts b){
     	System.out.println(t.transactionType);
     }
 
-    private static void changeplan(Transaction t, BankAccounts b){
+    public static void changeplan(Transaction t, BankAccounts b){
     	System.out.println(t.transactionType);
     }
 
-    private static void enable(Transaction t, BankAccounts b){
+    public static void enable(Transaction t, BankAccounts b){
     	System.out.println(t.transactionType);
     }
-    private static void login(Transaction t, BankAccounts b){
+    public static void login(Transaction t, BankAccounts b){
     	System.out.println(t.transactionType);
 	//check if it is an admin logging in
 	if (t.misc.compareTo("A ") == 0){
--- Phase4/source/backEnd.java
+++ Phase4/source/backEnd.java
@@ -16,10 +16,48 @@ public class backEnd{
 	ArrayList<Transaction> transactionsList = TransFileReader.read(args);
   
 	//while transactions still esits, process them
-
+	
 	int numTransactions = transactionsList.size();
 	for(int i = 0; i < numTransactions; i++){
-	    TransactionHandler.handle(transactionsList.get(i), bankAccounts);
+	    Transaction currTransaction = transactionsList.get(i);
+	    //TransactionHandler.handle(transactionsList.get(i), bankAccounts);
+	    	//Figure out the transaction type at question
+	    switch (currTransaction.transactionType) {
+	    case LOGOUT: 
+		TransactionHandler.logout(currTransaction, bankAccounts);
+		break;
+	    case WITHDRAWAL:
+		TransactionHandler.withdrawal(currTransaction, bankAccounts);
+		break;
+	    case TRANSFER:
+		Transaction nextTransaction = transactionsList.get(++i);
+		TransactionHandler.transfer(currTransaction, nextTransaction, bankAccounts);
+		break;
+	    case PAYBILL:
+		TransactionHandler.paybill(currTransaction, bankAccounts);
+		break;
+	    case DEPOSIT:
+		TransactionHandler.deposit(currTransaction, bankAccounts);
+		break;
+	    case CREATE:
+		TransactionHandler.create(currTransaction, bankAccounts);
+		break;
+	    case DELETE:
+		TransactionHandler.delete(currTransaction, bankAccounts);
+		break;
+	    case DISABLE: 
+		TransactionHandler.disable(currTransaction, bankAccounts);
+		break;
+	    case CHANGEPLAN: 
+		TransactionHandler.changeplan(currTransaction, bankAccounts);
+		break;
+	    case ENABLE:
+		TransactionHandler.enable(currTransaction, bankAccounts);
+		break;
+	    case LOGIN: 
+		TransactionHandler.login(currTransaction, bankAccounts);
+		break; 
+	    }
 	}
 
 	TransactionName test = TransactionName.values()[0];
